cbc229a55310b35bc148e9941d5029b67fc4b5fa
Tom Do <tom@sisuwellness.com>
2019-07-22 20:18:02 +1000

    edit name

--- src/chart/Rectangle.js
+++ src/chart/Rectangle.js
@@ -1,10 +1,12 @@
 import React, { useState } from 'react'
 import { ItemTypes } from './Constants'
 import { useDrag } from 'react-dnd';
+import { isAbsolute } from 'path';
 // import ContentEditable from 'react-contenteditable';
 
 // instanceid: 'shape1', shape: 'rectangle', name: 'process1', top: 10, left: 20
 export default function Rectangle({id, shape, name, left, top}) {
+
     const getStyles = (left, top, isDragging) => {
         const transform = `translate3d(${left}px, ${top}px, 0)`;
         return {
@@ -18,6 +20,8 @@ export default function Rectangle({id, shape, name, left, top}) {
             fontSize: 15,
             fontWeight: 'bold',
             cursor: 'move',
+            width: '108px',
+            height: '78px',    
         }
     }
 
@@ -28,29 +32,53 @@ export default function Rectangle({id, shape, name, left, top}) {
         }),
     })
 
-    let contentEditable = React.createRef();
     const [localName, setName] = useState( 'Shape 1' );
+    const [showShapeName, setShowShapeName] = useState( false );
 
     const handleChange = evt => {
-        alert('New value ' + evt.target);
         setName(evt.target.value);
     }
 
-  return (
-    <div ref={drag} style={getStyles(left, top, isDragging)}>
-        <svg width="106" height="76" >
-            <rect width="100" height="70" style={{fill: 'rgb(92, 155, 211)', 'strokeWidth':3, stroke: 'rgb(70, 118, 159)'}} />
-            {/* <ContentEditable
-              innerRef={contentEditable}
-              html={this.state.html} // innerHTML of the editable div
-              disabled={false}       // use true to disable editing
-              onChange={this.handleChange} // handle innerHTML change
-              tagName='text' // Use a custom HTML tag (uses a div by default)
-            /> */}
-            <text textAnchor="middle" x="50" y="40" fill="white" contentEditable="true" onInput={handleChange}>{localName}</text>
-            Sorry, your browser does not support inline SVG.  
-        </svg>
-    </div>
-  
-  )
+    const svgStyles = {
+        position: 'absolute',
+        top: '0',
+        left: '0',
+        'z-index': '8',
+        width: '106px',
+        height: '76px',
+    };
+
+    const shapeNameStyles = {
+        position: 'absolute',
+        'z-index': '9',
+        top: '25px',
+        left: '3px',
+        width: '88px',
+        display: showShapeName ? 'block' : 'none'
+    }
+
+    const nameClick = () => {
+        setShowShapeName(true);
+    }
+    
+    const handleKeyDown = (e) => {
+        if (e.key === 'Enter') {
+            setShowShapeName(false);
+        }
+    }
+
+    const hideNameInput = () => {
+        setShowShapeName(false);
+    }
+
+    return (
+        <div ref={drag} style={getStyles(left, top, isDragging)}>
+            <svg style={svgStyles}>
+                <rect width="100" height="70" style={{fill: 'rgb(92, 155, 211)', 'strokeWidth':3, stroke: 'rgb(70, 118, 159)'}} />
+                <text textAnchor="middle" x="50" y="40" fill="white" onClick={nameClick}>{localName}</text>
+            </svg>
+            <input value={localName} onChange={handleChange} style={shapeNameStyles} onKeyDown={handleKeyDown} onBlur={hideNameInput}></input>
+        </div>
+    
+    )
 }
